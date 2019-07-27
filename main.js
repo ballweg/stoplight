d00100b8ec1a669184550a9420f003454de6a773
Jeff Ballweg <ballweg@users.noreply.github.com>
2019-07-27 10:04:07 +0100

    remove loops, fixed to three lamps

--- main.js
+++ main.js
@@ -67,32 +67,32 @@ function setColors(redOn, yelOn, grnOn, lamps){
 
   var colors = '';
   // Red
-  for (var i = 0; i < lamps; i++) {
+//   for (var i = 0; i < lamps; i++) {
     if(redOn){
       colors += r;
     } else {
       colors += x;
     }
-  }
+//   }
 
   // Yellow
-  for (var i = 0; i < lamps; i++) {
+//   for (var i = 0; i < lamps; i++) {
     if(yelOn){
       colors += y;
     } else {
       colors += x;
     }
-  }
+//   }
 
   // Green
-  for (var i = 0; i < lamps; i++) {
+//   for (var i = 0; i < lamps; i++) {
     if(grnOn){
       colors += g;
     } else {
       colors += x;
     }
-  }
-
+//   }
+  console.log('writing ' + colors);
   updateUI(redOn, yelOn, grnOn);
   return colors;
 }
